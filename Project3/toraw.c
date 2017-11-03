cb1ed87509db92b5d6caceaaed8926c2b7bd639d
Rupal Shah <rupal.shah@uky.edu>
2017-11-03 12:14:17 -0400

    allows user to input multiple lines (ctrl+d to stop input)

--- Project3/toraw.c
+++ Project3/toraw.c
@@ -1,69 +1,73 @@
 #include <string.h>
 #include <stdio.h>
 
-int main(){
+int main() {
 
 	int val = 0;
 	char *line = NULL;
 	size_t size = 0;
+	int charDone = 0;
 
 	printf("Please start your input: \n");
 
-	//WHILE NOT END OF FILE
-	//while getline != null?
 
-	//parse each line of user's input, until they ctrl+d
-	getline(&line, &size, stdin);
+	// ALLOWS USER TO KEEP GOING UNTIL CTRL - D
+	// parse each line of user's input
+	while(getline(&line, &size, stdin) != -1){
 	
-	// ignores spaces in beginning of line
-	while(*line == ' '){
-		line++;  // increment index of character array
-	}
-
-	if(*line == '-'){  // if the character is a hyphen
-		line++;  // increment index of character array
-	
-		if(*line == 'c'){  // if next character is c
-
+		// ignores spaces in beginning of line
+		while(*line == ' ') {
 			line++;  // increment index of character array
+		}
 
-			// ignore all spaces after "-c"
-			while(*line == ' '){
-				line++;
+		if(*line == '-') {  // if the character is a hyphen
+			line++;  // increment index of character array
+	
+			if(*line == 'c') {  // if next character is c
+
+				line++;  // increment index of character array
+	
+				// ignore all spaces after "-c"
+				while(*line == ' ') {
+					line++;
+				}
+	
+	
+				// COMMENTS
+	
+				// print all characters after "-c"
+				printf("%s", line);
+	
 			}
-		
-			// print all characters after "-c"
-			printf("%s", line);
-
+	
+			else if(*line == 'd') {
+					
+				line++;  // increment index of character array
+					
+				char* token = strtok(line, " \n");  // splits string by space and new line
+				while(token != NULL) {
+	
+					sscanf(token, "%d", &val);  // stores numbers in val
+					printf("%c", val);  // converts int to character
+					token = strtok(NULL, " \n");  // re-assigns token to new delimited string
+				}
+			}
 		}
 	
-		else if(*line == 'd'){
-				
-			line++;  // increment index of character array
+		else {  // assume hexadecimal if no hyphen
 				
 			char* token = strtok(line, " \n");  // splits string by space and new line
-			while(token != NULL){
-				sscanf(token, "%d", &val);  // stores numbers in val
-				printf("%c", val);  // converts int to character
-				token = strtok(NULL, " \n");  // re-assigns token to new delimited string
-			}
-		}
-	}
-	
-	else{  // assume hexadecimal if no hyphen
-				
-		char* token = strtok(line, " \n");  // splits string by space and new line
-                while(token != NULL){
-                	sscanf(token, "%x", &val);  // stores numbers in val
-                  	printf("%c", val);  // converts hex to character
-                        token = strtok(NULL, " \n");  // re-assigns token to new delimited string
-		}						
-	
-
-	}
+       		        while(token != NULL){
+                		sscanf(token, "%x", &val);  // stores numbers in val
+	                  	printf("%c", val);  // converts hex to character
+       		                token = strtok(NULL, " \n");  // re-assigns token to new delimited string
+			}						
 		
-	
-	printf("end\n");
+
+		}
+		
+	}
+		printf("\nend\n");
 	
 
 }
