f9ef301227a3535985c32d49dc9251b1e1b6ac16
Rupal Shah <rupal.shah@uky.edu>
2017-11-05 16:01:39 -0500

    added comments to program

--- Project3/toraw.c
+++ Project3/toraw.c
@@ -1,98 +1,121 @@
+// Author: Rupal Shah
+
 #include <string.h>
 #include <stdio.h>
 #include <ctype.h>
 
 int main() {
 
-	int val = 0;
-	char *line = NULL;
-	int index = 0;
-	size_t size = 0;
-	int charDone = 0;
 
-	// set errors to false
-	int decError = 0;
-	char *decErrorMsg = NULL;
-	int hexError = 0;	
-	char *hexErrorMsg = NULL;
+	/* GENERAL COMMENTS:
+		-- 'line' is an array of characters (AKA a string)
+		-- 'line++' increments the index of the array being pointed to
+		-- '*line' dereferences the current character being pointed to
+	 */
+
+	// used to store each line of input 
+	char *line = NULL;
+	size_t size = 0;
+
+	// used to store string of output
 	char output[1024] = "";
 	size_t len = 0;
+	
+	// set errors to false
+	int decError = 0;
+	int hexError = 0;
 
+	// create strings to hold which input value is giving error
+	char *decErrorMsg = NULL;
+	char *hexErrorMsg = NULL;
+
+	// used to store decimal/hex values
+	int val = 0;
+
+	// prompt user to input
 	printf("Please start your input: \n");
 
-
-	// ALLOWS USER TO KEEP GOING UNTIL CTRL - D
-	// parse each line of user's input
-	while(getline(&line, &size, stdin) != -1){
+	// allow user to input lines until they press ctrl+d
+	// parse each line of user's input and store in 'line'
+	while(getline(&line, &size, stdin) != -1) {
 
 		// ignores spaces in beginning of line
 		while(*line == ' ') {
-			line++;  // increment index of character array
+			line++;
 		}
 
-		if(*line == '-') {  // if the character is a hyphen
-			line++;  // increment index of character array
+		// if character is hyphen
+		if(*line == '-') { 
+			line++;  
 				
-			// ignore all spaces after -
+			// ignore all spaces after hyphen
 			while(*line == ' ') {
 				line++;
 			}
 	
-			if(*line == 'c') {  // if next character is c
+			// if next character is 'c'
+			if(*line == 'c') { 
 
-				line++;  // increment index of character array
+				line++;
 	
-				// ignore all spaces after "-c"
+				// ignore all spaces after '-c'
 				while(*line == ' ') {
 					line++;
 				}
 
 				// ignore comments
-				char* token = strtok(line, "\n");  // only delimit by new line
-				for(int i = 0; i < strlen(token); i++){
-					if(token[i] == '/' && token[i + 1] == '*'){
-						while(token[i] != '*' || token[i + 1] != '/') {
-							i++;
+				char* token = strtok(line, "\n");  // separate line by new lines ONLY
+				for(int i = 0; i < strlen(token); i++){  // iterate through each character in token (line)
+					if(token[i] == '/' && token[i + 1] == '*'){  // if begining of comment is detected
+						while(token[i] != '*' || token[i + 1] != '/') {  // until end of comment
+							i++;  // do nothing (ignore characters in between)
 						}
-					i += 2;  // account for */
+					i += 2;  // account for '*/'
 					}
+
+					// add characters to output string
 					output[len++] = token[i];
 					output[len] = '\0';
+
 				}
 
-		}
+			}
 	
+			// if character after hyphen is 'd'
 			else if(*line == 'd') {
 					
-				line++;  // increment index of character array
+				line++;  
 					
-				char* token = strtok(line, " \n");  // splits string by space and new line
+				// split line into substring tokens (splits by space or new line)
+				char* token = strtok(line, " \n");
+
+				// until end of line
 				while(token != NULL) {
 	
 					// ignore comments
-					if(token[0] == '/' && token[1] == '*') {
-						token = strtok(NULL, " \n");
-						while(token[0] != '*' && token[1] != '/') {
-							token = strtok(NULL, " \n");
+					if(token[0] == '/' && token[1] == '*') {  // if beginning of comment is detected
+
+						token = strtok(NULL, " \n");  // go to next token
+						while(token[0] != '*' && token[1] != '/') {  // until token is end of comment
+							token = strtok(NULL, " \n");  // go to next token
 						}
 				
-						token = strtok(NULL, " \n");
+						token = strtok(NULL, " \n");  // goes to next token after comment (if applicable)
 						
 					}
 					
-					if(token != NULL) {  // accounts for if comment ends at end of line
-						// print error if input is not a digit
+					if(token != NULL) {  // accounts for if comment ends at end of line (if not at end of line...)
+
+						// iterate through each character of current token
 						for(int i = 0; i < strlen(token); i++) {
-							if(!isdigit(token[i])){
-//								printf("ERROR: NOT DECIMAL");
-								decError = 1;
-								decErrorMsg = token;
+							if(!isdigit(token[i])){  // if token includes non-digit
+								decError = 1;  // store that there is an error with the decimal input
+								decErrorMsg = token;  // store token that includes non-digit character
 							}
 						}
 
-						sscanf(token, "%d", &val);  // stores numbers in val
-//						printf("%c", val);  // converts int to character
-						output[len++] = (char)val;
+						sscanf(token, "%d", &val);  // converts strings of numbers into actual integers
+						output[len++] = (char)val;  // converts integers into characters (and stores result in output string)
 						output[len] = '\0';
 						token = strtok(NULL, " \n");  // re-assigns token to new delimited string
 					}
@@ -102,40 +125,48 @@ int main() {
 	
 		else {  // assume hexadecimal if no hyphen
 				
-			char* token = strtok(line, " \n");  // splits string by space and new line
+			// split line into substring tokens (splits by spaces or new line)		
+			char* token = strtok(line, " \n");  
+
+			// until end of line
        		        while(token != NULL){
 				
 				// ignores comments
-                		if(token[0] == '/' && token[1] == '*') { 
-					token = strtok(NULL, " \n");
-					while(token[0] != '*' & token[1] != '/') {
-						token = strtok(NULL, " \n");
+                		if(token[0] == '/' && token[1] == '*') {  // if beginning of comment is detected
+
+					token = strtok(NULL, " \n");  // go to next token
+					while(token[0] != '*' & token[1] != '/') {  // until token is end of comment
+						token = strtok(NULL, " \n");  // go to next token
 					}
-					token = strtok(NULL, " \n");
+
+					token = strtok(NULL, " \n");  // goes to next token after comment (if applicable)
+
 				}
 
-				if(token != NULL){
-					// print error is input is not a digit or letter (not hex - must be 2 digits/letters)
+				if(token != NULL){  // accounts for if comment ends at end of line (if not at end of line...)
+			
+					// iterate through each character of current token
 					for(int i = 0; i < strlen(token); i++) {
+
+						// if the character is not a digit, nor a letter, OR is notof length two
 						if((!isdigit(token[i]) && !isalpha(token[i])) ||  (strlen(token) != 2)) {
-//							printf("ERROR: NOT HEX");
-							hexError = 1;
-							hexErrorMsg = token;
+							hexError = 1;  // store that there is an error with the hexadecimal input
+							hexErrorMsg = token;  // store token with error
 						} 
 					}				
 	
-					sscanf(token, "%x", &val);  // stores numbers in val
-//		       	           	printf("%c", val);  // converts hex to character
-					output[len++] = (char)val;
+					sscanf(token, "%x", &val);  // converts string of numbers/letters into actual hexadecimal values
+					output[len++] = (char)val;  // converts hex into characters (and stores them in output string)
 					output[len] = '\0';
      			                token = strtok(NULL, " \n");  // re-assigns token to new delimited string
 				}
-			}								
-
-		}
 		
+			}								
+		}	
 	}
 	
+	// AFTER ALL LINES HAVE BEEN READ
+	// if there are errors, output the error and which decimal/hexadecimal caused the error
 	if(decError == 1 && hexError == 1) {
 		printf("ERROR: %s is not a decimal number.", decErrorMsg); 
 		printf("ERROR: %s is not a hexadecimal number.", hexErrorMsg);
@@ -146,6 +177,7 @@ int main() {
 	else if(hexError == 1) {
 		printf("ERROR: %s is not a hexadecimal number.", hexErrorMsg);
 	}
+	// if there are no errors, print all conversion of characters, decimals, and hexadecimals given into characters on same line
 	else {
 		for(int k = 0; k < 1024; k++) {
 			printf("%c", output[k]);
