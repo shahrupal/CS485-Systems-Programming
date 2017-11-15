57a494c5c9af2d1dd0ac9498345bbd6d655c3499
Rupal Shah <rupal.shah@uky.edu>
2017-11-14 20:33:49 -0500

    set/show commands work correctly

--- Project4/mysh.cpp
+++ Project4/mysh.cpp
@@ -4,35 +4,79 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <map>
 
 using namespace std;
 
 int main(){
 	
 	string input = "";
+	map<string,string> variables;
 
-	cout << "ur wish is my command: \n" << endl;	
+	cout << "ur wish is my command:" << endl;	
 
 	while(getline(cin, input)){
 
 		vector<string> tokens;
 		string temp = "";
 
+		// splits and stores line of input by spaces into vector
 		stringstream ss(input);
 		while(getline(ss, temp, ' ')){
 			tokens.push_back(temp);
 		}		
 
-		for(int i = 0; i < tokens.size(); i++){
-			cout << tokens[i];
+		// prints vector
+//		for(int i = 0; i < tokens.size(); i++){
+//			cout << tokens[i];
+//		}
+
+		// check size of vectors to make sure appropriate number of parameters associated with specific command
+		if(tokens[0] == "set"){ 
+			
+			if(tokens.size() == 3){
+				variables.insert(pair<string,string>(tokens[1],tokens[2]));  // store variables and their values into map
+			}
+			else{
+				cout << "The 'set' command requires 2 parameters. Use the following syntax: <set> <variable> <value>." << endl;
+			}
+
+
 		}
-		if(input == "bye"){
-			break;			
+		else if(tokens[0] == "show"){
+		
+			if(tokens.size() == 2){
+
+				// if variable not in map
+				if(variables.find(tokens[1]) == variables.end()){
+					cout << "Variable was not assigned a value." << endl;
+				}
+				// if variable in map, print out value associated with variable
+				else{
+					cout << variables.find(tokens[1])->second << endl;
+				}
+			}
+			else{
+				cout << "The 'show' command requres 1 paramete. Use the following syntax: <show> <variable>." << endl;
+			}
+
+		 }
+		else if(tokens[0] == "setprompt"){ }
+		else if(tokens[0] == "cd"){ }
+		else if(tokens[0] == "listp"){ }
+		else if(tokens[0] == "bye"){ 
+			break; 
+		}
+		else if(tokens[0] == "cmd") {
+			 // two options 
+		}
+		else {
+			cout << "Command not found. Please try again." << endl;
 		}
 
 	}
 
 
-	//exit status 0
+	return 0;
 
 }
